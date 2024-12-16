5eed86134daaace2354946b65aed37cbe9494992
Lina Marcela Sánchez Morales <lsanchezmo@unal.edu.co>
2024-12-16 14:15:54 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,13 +1,26 @@
 package vehiculos;
 
-import java.util.ArrayList;
-
 public class Pais {
-
-    public int contador;
-    public String nombre;
+    private static Pais[] lista = { null };
+    private String nombre;
+    public int exportacion = 0;
 
     public Pais(String nombre) {
+        this.nombre = nombre;
+        if (lista[0] == null) {
+            lista[0] = this;
+        } else {
+            int x = lista.length;
+            Pais[] A = new Pais[x + 1];
+            for (int i = 0; i < lista.length; i++) {
+                A[i] = lista[i];
+            }
+            A[x] = this;
+            lista = A;
+        }
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
@@ -15,29 +28,17 @@ public class Pais {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
     public static Pais paisMasVendedor() {
-        ArrayList<Fabricante> listadoFabricantes = Fabricante.getListadoFabricantes();
-        int maximo = 0;
-        Pais paisMaximo = null;
-        for(Fabricante f : listadoFabricantes){
-            if (f.getPais().getContador() >= maximo) { 
-                maximo = f.getPais().getContador();
-                paisMaximo = f.getPais();
+        Pais potencia = null;
+        for (Pais pais : lista) {
+            if (potencia != null) {
+                if (pais.exportacion > potencia.exportacion) {
+                    potencia = pais;
+                }
+            } else {
+                potencia = lista[0];
             }
         }
-        return paisMaximo;
+        return potencia;
     }
-
-    public int getContador() {
-        return contador;
-    }
-
-    public void setContador(int contador) {
-        this.contador = contador;
-    }
-
 }
