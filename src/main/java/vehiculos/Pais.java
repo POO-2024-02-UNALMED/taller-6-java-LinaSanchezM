bcf0a177592ec0ada5cbebb8e243dd9b02c7dba8
Lina Marcela Sánchez Morales <lsanchezmo@unal.edu.co>
2024-12-16 13:52:20 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -3,53 +3,41 @@ package vehiculos;
 import java.util.ArrayList;
 
 public class Pais {
-    private String nombre;
+
+    public int contador;
+    public String nombre;
 
     public Pais(String nombre) {
         this.nombre = nombre;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public String getNombre() {
-        return this.nombre;
-    }
-
+    
     public static Pais paisMasVendedor() {
-        ArrayList<String> paises = new ArrayList<>();
-        String ganador = masRepetido(paises);
-        for (int i = 0; i < Automovil.vehiculos.size(); i++) {
-            Vehiculo cuenta = Automovil.vehiculos.get(i);
-            String str = cuenta.fabricante.getPais().getNombre();
-            paises.add(str);
-        }
-        for (int i = 0; i < Automovil.vehiculos.size(); i++) {
-            Vehiculo cuenta2 = Automovil.vehiculos.get(i);
-            if (cuenta2.fabricante.getPais().getNombre() == ganador) {
-                Pais winer = cuenta2.fabricante.getPais();
-                return winer;
-            } 
-        }
-        return null;
-    }
-
-    public static String masRepetido(ArrayList<String> paises) {
-        String masRepetido = null;
-        int maxRepeticiones = 0;
-        for (String elemento : paises) {
-            int repeticiones = 0;
-            for (String comparar : paises) {
-                if (elemento.equals(comparar)) {
-                    repeticiones++;
-                }
-                if (repeticiones > maxRepeticiones) {
-                    maxRepeticiones = repeticiones;
-                    masRepetido = elemento;
-                }
+        ArrayList<Fabricante> listadoFabricantes = Fabricante.getListadoFabricantes();
+        int maximo = 0;
+        Pais paisMaximo = null;
+        for(Fabricante f : listadoFabricantes){
+            if (f.getPais().getContador() >= maximo) { 
+                maximo = f.getPais().getContador();
+                paisMaximo = f.getPais();
             }
         }
-        return masRepetido;
+        return paisMaximo;
     }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
 }
